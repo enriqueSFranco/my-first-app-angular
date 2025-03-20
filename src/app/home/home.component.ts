72699a6c98efd2f30b86501373fe94924b3dd279
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-03-20 12:48:01 -0600

    homes app

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form action="">
+        <input type="text" placeholder="filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          search
+        </button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+
+  filterResults(text: string) {
+    console.log(`Searching: ${text}`);
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) => {
+        return housingLocation.city.toLowerCase().includes(text.toLowerCase());
+      }
+    );
+    console.log(this.filteredLocationList)
+  }
+}
