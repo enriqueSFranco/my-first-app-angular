4bc9a8317138abfd59f5aa796efc3426b4a09ced
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-05-13 15:14:55 -0600

    feat: module shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SharedComponent } from './shared.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { NotificationModule } from '../notification/notification.module';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, MenuComponent, SharedComponent],
+  imports: [CommonModule, NotificationModule, FormsModule],
+  exports: [SharedComponent],
+})
+export class SharedModule {}
