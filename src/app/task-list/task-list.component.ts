207bf5f59bebe9beb546ee63a3a580cbfcb53996
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-05-20 16:02:06 -0600

    feat: componente que muestra una lista de tareas

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,27 +1,56 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Task } from './interfaces/task';
+import { Task, TaskPriority, TaskStatus } from './interfaces/task';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgIf, NgFor, NgSwitch, NgSwitchCase],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
-  prioritySelected = '';
-  statusSelected = '';
+  showClassSale = false; // se usa para agregar una clase de forma dinamica al componente [class.sale]="showClassSale"
+  // NOTE: en este coso la clase .sale no se agregara por que esta en false.
 
-  task: Task = {
-    id: crypto.randomUUID(),
-    title: 'Hacer ejercicio',
-    status: 'pending',
-    priority: 'low',
-  };
+  // enum TaskStatus
+  TaskStatus = TaskStatus;
 
-  getBgColorByPriority(priority: 'high' | 'medium' | 'low') {
+  readonly statuses = [
+    { value: TaskStatus.Progress, label: 'Progress' },
+    { value: TaskStatus.Pending, label: 'Pending' },
+    { value: TaskStatus.Completed, label: 'Completed' },
+  ];
+
+  readonly priorities = [
+    { value: TaskPriority.Low, label: 'Low' },
+    { value: TaskPriority.Medium, label: 'Medium' },
+    { value: TaskPriority.High, label: 'High' },
+  ];
+
+  testTasks = [
+    {
+      id: crypto.randomUUID(),
+      title: 'Hacer ejercicio',
+      status: TaskStatus.Progress,
+      priority: TaskPriority.High,
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Ir de comprar al super',
+      status: TaskStatus.Pending,
+      priority: TaskPriority.Low,
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Salir de pase con mi mascota',
+      status: TaskStatus.Pending,
+      priority: TaskPriority.Medium,
+    },
+  ];
+
+  getBgColorByPriority(priority: TaskPriority) {
     switch (priority) {
       case 'high':
         return '#ffccd5';
@@ -34,7 +63,20 @@ export class TaskListComponent {
     }
   }
 
-  changeStatus() {
-    console.log(this.task)
+  getStatusClass(status: TaskStatus) {
+    return {
+      [TaskStatus.Progress]: 'task--status-progress',
+      [TaskStatus.Pending]: 'task--status-pending',
+      [TaskStatus.Completed]: 'task--status-completed',
+    }[status];
+  }
+
+  trackByTaskId(index: number, task: Task) {
+    return task.id;
+  }
+
+  onStatusChange(task: Task, newStatus: TaskStatus) {
+    task.status = newStatus;
+    console.log(this.testTasks);
   }
 }
