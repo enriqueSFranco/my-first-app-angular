e43e7ab30479256a201f93ce9570225f118550c8
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-05-19 19:09:10 -0600

    feat: uso del componente task-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+import { TaskListComponent } from "./task-list/task-list.component";
 
 // import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root', // como se va a referir angular al componente
   standalone: true, // para describir si el componente requiere a NgModule
-  imports: [SharedModule], // describe las dependencias del componente
+  imports: [SharedModule, TaskListComponent], // describe las dependencias del componente
   templateUrl: `./app.component.html`, // diseño del componente
   styleUrl: './app.component.css', // para enumerar los estilos que requiere el componente
 })
